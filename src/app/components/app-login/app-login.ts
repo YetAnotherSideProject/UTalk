f4c53582ebf69d67dc25d3d6e12d20fee06ada6b
Martin Schulze Beckendorf <martin.sb@outlook.de>
2020-08-14 12:40:40 +0200

    Login: Small Login/Register Flow and UI Changes

--- src/app/components/app-login/app-login.ts
+++ src/app/components/app-login/app-login.ts
@@ -1,10 +1,22 @@
-import { LitElement, customElement, html, query } from "lit-element";
+import {
+  LitElement,
+  customElement,
+  html,
+  internalProperty,
+  query,
+} from "lit-element";
 import { AuthService } from "../../services/AuthService";
 
 @customElement("app-login")
 class AppLogin extends LitElement {
-  @query("#email") emailInput!: HTMLInputElement;
-  @query("#password") passwordInput!: HTMLInputElement;
+  @internalProperty()
+  doRegister: boolean = false;
+  @query("#email")
+  emailInput!: HTMLInputElement;
+  @query("#password")
+  passwordInput!: HTMLInputElement;
+  @query("#password_again")
+  passwordAgainInput!: HTMLInputElement;
 
   constructor() {
     super();
@@ -12,36 +24,98 @@ class AppLogin extends LitElement {
 
   render() {
     return html`
-      <ion-content class="ion-padding">
-        <h1>Login or Register</h1>
-        <ion-item-group>
-          <ion-item>
-            <ion-label position="floating">E-Mail</ion-label>
-            <ion-input type="email" id="email" required></ion-input>
+      <ion-header>
+        <ion-toolbar>
+          <ion-title>${this.doRegister ? "Register" : "Login"}</ion-title>
+        </ion-toolbar>
+      </ion-header>
+
+      <ion-content>
+        <!--onsubmit="return false", damit die Form kein Redirect jeglicher Art macht oder Daten versendet z.B. in die URL -->
+        <form onsubmit="return false" @submit=${() => this.onSubmit()}>
+          <ion-item lines="full">
+            <ion-label position="floating">Email</ion-label>
+            <ion-input type="text" id="email" required></ion-input>
           </ion-item>
-          <ion-item>
+
+          <ion-item lines="full">
             <ion-label position="floating">Password</ion-label>
             <ion-input type="password" id="password" required></ion-input>
           </ion-item>
-          <ion-item-divider></ion-item-divider>
-        </ion-item-group>
-        <ion-button-row>
-          <ion-button @click=${() => this.login()}>Login</ion-button>
-          <ion-button @click=${() => this.register()}>Register</ion-button>
-        </ion-button-row>
+          ${this.doRegister
+            ? html`<ion-item lines="full">
+                <ion-label position="floating">Password again</ion-label>
+                <ion-input
+                  type="password"
+                  id="password_again"
+                  required
+                ></ion-input>
+              </ion-item>`
+            : html``}
+
+          <ion-row>
+            <ion-col>
+              <ion-button type="submit" expand="block"
+                >${this.doRegister ? "Register" : "Signin"}</ion-button
+              >
+            </ion-col>
+          </ion-row>
+        </form>
+        ${!this.doRegister
+          ? html`<ion-row>
+              <ion-col>
+                <ion-button
+                  @click=${() => (this.doRegister = true)}
+                  expand="block"
+                  >Create Account</ion-button
+                >
+              </ion-col>
+            </ion-row>`
+          : html`<ion-row>
+              <ion-col>
+                <ion-button
+                  @click=${() => (this.doRegister = false)}
+                  expand="block"
+                  >Instead Signin?</ion-button
+                >
+              </ion-col>
+            </ion-row>`}
       </ion-content>
     `;
   }
 
-  async login() {
-    await AuthService.login(this.emailInput.value, this.passwordInput.value)
-      .then(() => console.log(`Logged in`))
-      .catch(() => console.log("Loggin failed"));
+  async onSubmit() {
+    if (this.doRegister) {
+      if (this.passwordInput.value !== this.passwordAgainInput.value) {
+        this.showToast(`Passwords do not match!`);
+        this.passwordInput.value = ``;
+        this.passwordAgainInput.value = ``;
+      } else {
+        await AuthService.register(
+          this.emailInput.value,
+          this.passwordInput.value
+        )
+          .then(() => this.showToast(`User registered and logged in!`))
+          .catch(() => {
+            this.showToast(`Register failed!`);
+            this.passwordInput.value = ``;
+            this.passwordAgainInput.value = ``;
+            this.emailInput.value = ``;
+          });
+      }
+    } else {
+      await AuthService.login(this.emailInput.value, this.passwordInput.value)
+        .then(() => this.showToast(`Logged in`))
+        .catch(() => this.showToast("Login failed"));
+    }
   }
 
-  async register() {
-    await AuthService.register(this.emailInput.value, this.passwordInput.value)
-      .then(() => console.log(`User registered`))
-      .catch(() => console.log(`Register failed`));
+  async showToast(message: string) {
+    const toast = document.createElement("ion-toast");
+    toast.message = message;
+    toast.duration = 2000;
+
+    document.body.appendChild(toast);
+    return toast.present();
   }
 }
