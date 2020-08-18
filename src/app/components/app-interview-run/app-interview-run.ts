03d50feefabb99a84d601e428aad4b6d507dc913
Mario Kiese <mali.mk73@googlemail.com>
2020-08-18 18:10:53 +0200

    First layout for InterviewRun View

--- src/app/components/app-interview-run/app-interview-run.ts
+++ src/app/components/app-interview-run/app-interview-run.ts
@@ -7,12 +7,96 @@ class AppRunInterview extends LitElement {
   }
 
   render() {
+    // TODO
+    // Responsiv machen
     return html`
       <app-toolbar
         customBackButton="true"
         .customClick=${() => this.navigateBack()}
       ></app-toolbar>
-      <ion-content class="padding"><h1>Run Interview</h1> </ion-content>
+      <ion-content class="padding">
+        <ion-card>
+          <ion-card-header>
+            <ion-card-subtitle>Interview</ion-card-subtitle>
+            <ion-card-title>Tesla Pressekonferenz 09/2020</ion-card-title>
+          </ion-card-header>
+          <ion-card-content
+            >Interview Part: 4/8 - Neuentwicklungen</ion-card-content
+          >
+          <ion-progress-bar value="0.5"></ion-progress-bar>
+        </ion-card>
+        <ion-slides>
+          <ion-slide>
+            <ion-card style="height: 200px; background-color: lightGray">
+              <ion-card-header>
+                <ion-card-subtitle>Frage</ion-card-subtitle>
+              </ion-card-header>
+              <ion-card-content
+                >Welche Neuentwicklungen wird es in den kommenden 5 Jahren
+                geben?</ion-card-content
+              >
+            </ion-card>
+          </ion-slide>
+          <ion-slide>
+            <ion-card>
+              <ion-card-header>
+                <ion-card-subtitle>Frage</ion-card-subtitle>
+              </ion-card-header>
+              <ion-card-content
+                >Welche Neuentwicklungen wird es in den kommenden 5 Jahren
+                geben?</ion-card-content
+              >
+            </ion-card>
+          </ion-slide>
+          <ion-slide>
+            <ion-card>
+              <ion-card-header>
+                <ion-card-subtitle>Frage</ion-card-subtitle>
+              </ion-card-header>
+              <ion-card-content
+                >Welche Neuentwicklungen wird es in den kommenden 5 Jahren
+                geben?</ion-card-content
+              >
+            </ion-card>
+          </ion-slide>
+          <ion-slide>
+            <ion-card>
+              <ion-card-header>
+                <ion-card-subtitle>Frage</ion-card-subtitle>
+              </ion-card-header>
+              <ion-card-content
+                >Welche Neuentwicklungen wird es in den kommenden 5 Jahren
+                geben?</ion-card-content
+              >
+            </ion-card>
+          </ion-slide>
+        </ion-slides>
+        <ion-card style="height: 185px">
+          <ion-card-header>
+            <ion-card-subtitle>Antwort</ion-card-subtitle>
+          </ion-card-header>
+          <ion-card-content>
+            <ion-textarea
+              placeholder="Gibt deine Antwort ein ..."
+            ></ion-textarea>
+          </ion-card-content>
+        </ion-card>
+        <div style="display: flex; justify-content: space-between">
+          <div style="display: flex; align-items: center">
+            <ion-icon
+              style="font-size: 4em; margin: 0px 20px;"
+              name="arrow-back-outline"
+            ></ion-icon>
+            <p>Prev</p>
+          </div>
+          <div style="display: flex; align-items: center"">
+          <p>Next</p>
+          <ion-icon
+            style="font-size: 4em; margin: 0px 20px"
+            name="arrow-forward-outline"
+          ></ion-icon>
+        </div>
+      </ion-content>
     `;
   }
 
