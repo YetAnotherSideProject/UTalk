e6bf73ebf745b2026a77d76a3ade15d08ad76d55
Mario Kiese <mali.mk73@googlemail.com>
2020-08-19 15:21:10 +0200

    Implemented logic for moving forwards and backwards throug the interview by using navigation arrows

--- src/app/components/app-interview-run/app-interview-run.ts
+++ src/app/components/app-interview-run/app-interview-run.ts
@@ -1,12 +1,43 @@
-import { LitElement, html, customElement } from "lit-element";
+import {
+  LitElement,
+  html,
+  css,
+  customElement,
+  property,
+  internalProperty,
+} from "lit-element";
+
+import { Interview } from "../../models/Interview";
 
 @customElement("app-interview-run")
 class AppRunInterview extends LitElement {
+  @property({ type: Object }) interview = {} as Interview;
+  questions: string[] = [];
+
+  @internalProperty() currentPart: number = 0;
+  @internalProperty() maxParts: number = 0;
+  @internalProperty() currentQuestionInPart: number = 0;
+  @internalProperty() maxQuestionsInPart: number = 0;
+  @internalProperty() currentQuestionTotal: number = 0;
+  @internalProperty() maxQuestions: number = 0;
+
   constructor() {
     super();
   }
 
+  static get styles() {
+    return css`
+      ion-button {
+        --background: white;
+        --background-activated: white;
+      }
+    `;
+  }
+
   render() {
+    const progress = (this.currentQuestionTotal + 1) / this.maxQuestions;
+
+    console.log("Progress: ", progress);
     // TODO
     // Responsiv machen
     return html`
@@ -18,58 +49,31 @@ class AppRunInterview extends LitElement {
         <ion-card>
           <ion-card-header>
             <ion-card-subtitle>Interview</ion-card-subtitle>
-            <ion-card-title>Tesla Pressekonferenz 09/2020</ion-card-title>
+            <ion-card-title>${this.interview.title}</ion-card-title>
           </ion-card-header>
           <ion-card-content
-            >Interview Part: 4/8 - Neuentwicklungen</ion-card-content
+            >Interview Part:
+            ${this.currentPart + 1}/${this.maxParts} -
+            ${
+              this.interview.interviewParts[this.currentPart].title
+            }</ion-card-content
           >
-          <ion-progress-bar value="0.5"></ion-progress-bar>
+          <ion-progress-bar value=${progress}></ion-progress-bar>
         </ion-card>
         <ion-slides>
-          <ion-slide>
-            <ion-card style="height: 200px; background-color: lightGray">
+        ${this.questions.map(
+          (question) => html`<ion-slide>
+            <ion-card
+              style="width: 100%; height: 200px; background-color: lightGray"
+            >
               <ion-card-header>
                 <ion-card-subtitle>Frage</ion-card-subtitle>
               </ion-card-header>
-              <ion-card-content
-                >Welche Neuentwicklungen wird es in den kommenden 5 Jahren
-                geben?</ion-card-content
-              >
+              <ion-card-content>${question}</ion-card-content>
             </ion-card>
-          </ion-slide>
-          <ion-slide>
-            <ion-card>
-              <ion-card-header>
-                <ion-card-subtitle>Frage</ion-card-subtitle>
-              </ion-card-header>
-              <ion-card-content
-                >Welche Neuentwicklungen wird es in den kommenden 5 Jahren
-                geben?</ion-card-content
-              >
-            </ion-card>
-          </ion-slide>
-          <ion-slide>
-            <ion-card>
-              <ion-card-header>
-                <ion-card-subtitle>Frage</ion-card-subtitle>
-              </ion-card-header>
-              <ion-card-content
-                >Welche Neuentwicklungen wird es in den kommenden 5 Jahren
-                geben?</ion-card-content
-              >
-            </ion-card>
-          </ion-slide>
-          <ion-slide>
-            <ion-card>
-              <ion-card-header>
-                <ion-card-subtitle>Frage</ion-card-subtitle>
-              </ion-card-header>
-              <ion-card-content
-                >Welche Neuentwicklungen wird es in den kommenden 5 Jahren
-                geben?</ion-card-content
-              >
-            </ion-card>
-          </ion-slide>
+          </ion-slide>`
+        )}
+          
         </ion-slides>
         <ion-card style="height: 185px">
           <ion-card-header>
@@ -81,20 +85,24 @@ class AppRunInterview extends LitElement {
             ></ion-textarea>
           </ion-card-content>
         </ion-card>
-        <div style="display: flex; justify-content: space-between">
+        <div style="display: flex; justify-content: space-between; margin-top: 50px">
           <div style="display: flex; align-items: center">
+          <ion-button @click=${this.decreaseQuestionCounter}>
             <ion-icon
-              style="font-size: 4em; margin: 0px 20px;"
+              style="font-size: 4em; color: var(--ion-color-primary);"
               name="arrow-back-outline"
             ></ion-icon>
+          </ion-button>
             <p>Prev</p>
           </div>
-          <div style="display: flex; align-items: center"">
+          <div style="display: flex; align-items: center">
           <p>Next</p>
+          <ion-button @click=${this.increaseQuestionCounter}>
           <ion-icon
-            style="font-size: 4em; margin: 0px 20px"
+            style="font-size: 4em; color: var(--ion-color-primary);"
             name="arrow-forward-outline"
           ></ion-icon>
+          </ion-button>
         </div>
       </ion-content>
     `;
@@ -106,5 +114,74 @@ class AppRunInterview extends LitElement {
     ) as HTMLIonNavElement;
 
     nav.pop();
+  }
+
+  increaseQuestionCounter() {
+    if (this.currentQuestionInPart < this.maxQuestionsInPart - 1) {
+      this.currentQuestionInPart++;
+      this.currentQuestionTotal++;
+    } else {
+      if (this.currentPart < this.maxParts - 1) {
+        this.currentPart++;
+        this.currentQuestionInPart = 0;
+        this.currentQuestionTotal++;
+        this.maxQuestionsInPart = this.interview.interviewParts[
+          this.currentPart
+        ].interviewQuestions.length;
+      }
+    }
+
+    console.log("Current part: ", this.currentPart);
+    console.log("Current question in part: ", this.currentQuestionInPart);
+    console.log("Max questions in part: ", this.maxQuestionsInPart);
+    console.log("Current question total: ", this.currentQuestionTotal);
+    console.log("Max parts: ", this.maxParts);
+  }
+
+  decreaseQuestionCounter() {
+    if (this.currentQuestionInPart > 0) {
+      this.currentQuestionInPart--;
+      this.currentQuestionTotal--;
+    } else {
+      if (this.currentPart > 0) {
+        this.currentPart--;
+        this.maxQuestionsInPart = this.interview.interviewParts[
+          this.currentPart
+        ].interviewQuestions.length;
+        this.currentQuestionInPart = this.maxQuestionsInPart - 1;
+        this.currentQuestionTotal--;
+      }
+    }
+    console.log("Current part: ", this.currentPart);
+    console.log("Current question in part: ", this.currentQuestionInPart);
+    console.log("Max questions in part: ", this.maxQuestionsInPart);
+    console.log("Current question total: ", this.currentQuestionTotal);
+    console.log("Max parts: ", this.maxParts);
+  }
+
+  loadInitialData() {
+    this.maxQuestionsInPart = this.interview.interviewParts[
+      this.currentPart
+    ].interviewQuestions.length;
+    this.maxParts = this.interview.interviewParts.length;
+    this.maxQuestions = this.interview.interviewParts.reduce(
+      (amount, part) => amount + part.interviewQuestions.length,
+      0
+    );
+    this.questions = this.interview.interviewParts
+      .map((part) =>
+        part.interviewQuestions.map((question) => question.question)
+      )
+      .reduce((prev, current) => prev.concat(current));
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    this.addEventListener("ionViewWillEnter", this.loadInitialData);
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+    this.removeEventListener("ionViewWillEnter", this.loadInitialData);
   }
 }
