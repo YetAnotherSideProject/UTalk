a778709a250f08c86a736b0618871112d1f4943c
Mario Kiese <mali.mk73@googlemail.com>
2020-08-20 16:06:58 +0200

    New styling for login view

--- src/app/components/app-login/app-login.ts
+++ src/app/components/app-login/app-login.ts
@@ -52,66 +52,88 @@ class AppLogin extends LitElement {
       </ion-header>
 
       <ion-content>
-        <!--onsubmit="return false", damit die Form kein Redirect jeglicher Art macht oder Daten versendet z.B. in die URL -->
-        <form onsubmit="return false" @submit=${() => this.onSubmit()}>
-          <ion-item lines="full">
-            <ion-label position="floating">Email</ion-label>
-            <ion-input type="text" id="email" required></ion-input>
-          </ion-item>
+        <div
+          style="display: flex; flex-direction: column;  width: 100%; height: 100%"
+        >
+          <img
+            src="src/assets/img/interview.jpg"
+            width="100%"
+            height="100%"
+            style="object-fit: cover; position: absolute;"
+          />
 
-          <ion-item lines="full">
-            <ion-label position="floating">Password</ion-label>
-            <ion-input type="password" id="password" required></ion-input>
-          </ion-item>
-          ${this.doRegister
-            ? html`<ion-item lines="full">
-                <ion-label position="floating">Password again</ion-label>
-                <ion-input
-                  type="password"
-                  id="password_again"
-                  required
-                ></ion-input>
-              </ion-item>`
+          <img
+            src="src/assets/img/utalk_logo_white.png"
+            alt="uTalk logo"
+            width="100%"
+            style="z-index: 100"
+          />
+          <!--onsubmit="return false", damit die Form kein Redirect jeglicher Art macht oder Daten versendet z.B. in die URL -->
+          <div style="opacity: 0.7; padding: 1em">
+            <form onsubmit="return false" @submit=${() => this.onSubmit()}>
+              <ion-item lines="full">
+                <ion-label position="floating">Email</ion-label>
+                <ion-input type="text" id="email" required></ion-input>
+              </ion-item>
+
+              <ion-item lines="full">
+                <ion-label position="floating">Password</ion-label>
+                <ion-input type="password" id="password" required></ion-input>
+              </ion-item>
+              ${this.doRegister
+                ? html`<ion-item lines="full">
+                    <ion-label position="floating">Password again</ion-label>
+                    <ion-input
+                      type="password"
+                      id="password_again"
+                      required
+                    ></ion-input>
+                  </ion-item>`
+                : html``}
+
+              <ion-row style="margin-top: 2em">
+                <ion-col>
+                  <ion-button
+                    type="submit"
+                    expand="block"
+                    disabled=${!this.online}
+                    >${this.doRegister ? "Register" : "Signin"}</ion-button
+                  >
+                </ion-col>
+              </ion-row>
+            </form>
+            ${!this.doRegister
+              ? html`<ion-row>
+                  <ion-col>
+                    <ion-button
+                      @click=${() => (this.doRegister = true)}
+                      expand="block"
+                      >Create Account</ion-button
+                    >
+                  </ion-col>
+                </ion-row>`
+              : html`<ion-row>
+                  <ion-col>
+                    <ion-button
+                      @click=${() => (this.doRegister = false)}
+                      expand="block"
+                      >Instead Signin?</ion-button
+                    >
+                  </ion-col>
+                </ion-row>`}
+          </div>
+          ${!this.online
+            ? // TODO Nicht responsives Verhalten von bottom: 43px ändern. 0px funktioniert bisher nicht --> Ursachenforschung
+              html` <ion-footer style="position: absolute; bottom: 43px;">
+                <ion-toolbar id="footer-toolbar">
+                  <h1 style="font-size: 1.5em;">
+                    No connection detected!
+                  </h1>
+                  <p>Checking for reconnection...</p>
+                </ion-toolbar>
+              </ion-footer>`
             : html``}
-
-          <ion-row>
-            <ion-col>
-              <ion-button type="submit" expand="block" disabled=${!this.online}
-                >${this.doRegister ? "Register" : "Signin"}</ion-button
-              >
-            </ion-col>
-          </ion-row>
-        </form>
-        ${!this.doRegister
-          ? html`<ion-row>
-              <ion-col>
-                <ion-button
-                  @click=${() => (this.doRegister = true)}
-                  expand="block"
-                  >Create Account</ion-button
-                >
-              </ion-col>
-            </ion-row>`
-          : html`<ion-row>
-              <ion-col>
-                <ion-button
-                  @click=${() => (this.doRegister = false)}
-                  expand="block"
-                  >Instead Signin?</ion-button
-                >
-              </ion-col>
-            </ion-row>`}
-        ${!this.online
-          ? // TODO Nicht responsives Verhalten von bottom: 43px ändern. 0px funktioniert bisher nicht --> Ursachenforschung
-            html` <ion-footer style="position: absolute; bottom: 43px;">
-              <ion-toolbar id="footer-toolbar">
-                <h1 style="font-size: 1.5em;">
-                  No connection detected!
-                </h1>
-                <p>Checking for reconnection...</p>
-              </ion-toolbar>
-            </ion-footer>`
-          : html``}
+        </div>
       </ion-content>
     `;
   }
