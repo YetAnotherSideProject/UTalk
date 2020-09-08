d3b37b313552ba91541da5accb64f1b72063b8db
Mario Kiese <mali.mk73@googlemail.com>
2020-09-08 12:07:38 +0200

    Layout adaptions in login view to prevent overflowing and too wide form.

--- src/app/components/app-login/app-login.ts
+++ src/app/components/app-login/app-login.ts
@@ -42,12 +42,15 @@ class AppLogin extends LitElement {
       .login {
         display: flex;
         flex-direction: column;
+        align-items: center;
         width: 100%;
-      }
-      .login__backgroundImage {
-        object-fit: cover;
-        position: absolute;
         height: 100%;
+        position: relative;
+        background-image: url("src/assets/img/interview.jpg");
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: center;
+        overflow: scroll;
       }
       .login__logo {
         object-fit: contain;
@@ -57,9 +60,19 @@ class AppLogin extends LitElement {
       .login__form {
         opacity: 0.7;
         padding: 1em;
+        width: 100%;
+        max-width: 450px;
       }
       .login__signin {
         margin-top: 2em;
+      }
+      .login__signin ion-button {
+        --background: var(--ion-color-tertiary);
+        --color: white;
+      }
+      form {
+        margin-left: 1em;
+        margin-right: 1em;
       }
       form > ion-item {
         border-radius: 10px;
@@ -80,90 +93,87 @@ class AppLogin extends LitElement {
 
   render() {
     return html`
-      <ion-header>
-        <ion-toolbar>
-          <ion-title>${this.doRegister ? "Register" : "Login"}</ion-title>
-        </ion-toolbar>
-      </ion-header>
+      <div style="display: flex; flex-direction: column; height: 100%">
+        <ion-header>
+          <ion-toolbar>
+            <ion-title>${this.doRegister ? "Register" : "Login"}</ion-title>
+          </ion-toolbar>
+        </ion-header>
 
-      <ion-content>
-        <div class="login">
-          <img
-            class="login__backgroundImage"
-            src="${InterviewImage}"
-            width="100%"
-          />
+        <ion-content>
+          <div class="login">
+            <img
+              class="login__logo"
+              src="${UTalkLogo_White}"
+              alt="uTalk logo"
+              width="100%"
+            />
+            <!--onsubmit="return false", damit die Form kein Redirect jeglicher Art macht oder Daten versendet z.B. in die URL -->
+            <div class="login__form">
+              <form onsubmit="return false" @submit=${() => this.onSubmit()}>
+                <ion-item lines="full">
+                  <ion-label position="floating">Email</ion-label>
+                  <ion-input type="text" id="email" required></ion-input>
+                </ion-item>
 
-          <img
-            class="login__logo"
-            src="${UTalkLogo_White}"
-            alt="uTalk logo"
-            width="100%"
-          />
-          <!--onsubmit="return false", damit die Form kein Redirect jeglicher Art macht oder Daten versendet z.B. in die URL -->
-          <div class="login__form">
-            <form onsubmit="return false" @submit=${() => this.onSubmit()}>
-              <ion-item lines="full">
-                <ion-label position="floating">Email</ion-label>
-                <ion-input type="text" id="email" required></ion-input>
-              </ion-item>
+                <ion-item lines="full">
+                  <ion-label position="floating">Password</ion-label>
+                  <ion-input type="password" id="password" required></ion-input>
+                </ion-item>
+                ${this.doRegister
+                  ? html`<ion-item lines="full">
+                      <ion-label position="floating">Password again</ion-label>
+                      <ion-input
+                        type="password"
+                        id="password_again"
+                        required
+                      ></ion-input>
+                    </ion-item>`
+                  : html``}
 
-              <ion-item lines="full">
-                <ion-label position="floating">Password</ion-label>
-                <ion-input type="password" id="password" required></ion-input>
-              </ion-item>
-              ${this.doRegister
-                ? html`<ion-item lines="full">
-                    <ion-label position="floating">Password again</ion-label>
-                    <ion-input
-                      type="password"
-                      id="password_again"
-                      required
-                    ></ion-input>
-                  </ion-item>`
-                : html``}
-
-              <ion-row class="login__signin">
-                <ion-col>
-                  <ion-button
-                    type="submit"
-                    expand="block"
-                    disabled=${!this.online}
-                    >${this.doRegister ? "Register" : "Signin"}</ion-button
-                  >
-                </ion-col>
-              </ion-row>
-            </form>
-            ${!this.doRegister
-              ? html`<ion-row>
+                <ion-row class="login__signin">
                   <ion-col>
                     <ion-button
-                      @click=${() => (this.doRegister = true)}
+                      type="submit"
                       expand="block"
-                      >Create Account</ion-button
+                      disabled=${!this.online}
+                      >${this.doRegister ? "Register" : "Signin"}</ion-button
                     >
                   </ion-col>
-                </ion-row>`
-              : html`<ion-row>
-                  <ion-col>
-                    <ion-button
-                      @click=${() => (this.doRegister = false)}
-                      expand="block"
-                      >Instead Signin?</ion-button
-                    >
-                  </ion-col>
-                </ion-row>`}
+                </ion-row>
+
+                ${!this.doRegister
+                  ? html`<ion-row>
+                      <ion-col>
+                        <ion-button
+                          @click=${() => (this.doRegister = true)}
+                          expand="block"
+                          >Create Account</ion-button
+                        >
+                      </ion-col>
+                    </ion-row>`
+                  : html`<ion-row>
+                      <ion-col>
+                        <ion-button
+                          @click=${() => (this.doRegister = false)}
+                          expand="block"
+                          >Instead Signin?</ion-button
+                        >
+                      </ion-col>
+                    </ion-row>`}
+              </form>
+            </div>
+            ${!this.online
+              ? html` <ion-footer class="login__footer">
+                  <ion-toolbar class="login__footerToolbar">
+                    <h1 class="login__footerText">No connection detected!</h1>
+                    <p>Checking for reconnection...</p>
+                  </ion-toolbar>
+                </ion-footer>`
+              : html``}
           </div>
-          ${!this.online
-            ? html` <ion-footer class="login__footer">
-                <ion-toolbar class="login__footerToolbar">
-                  <h1 class="login__footerText">No connection detected!</h1>
-                  <p>Checking for reconnection...</p>
-                </ion-toolbar>
-              </ion-footer>`
-            : html``}
-        </div>
-      </ion-content>
+        </ion-content>
+      </div>
     `;
   }
 
